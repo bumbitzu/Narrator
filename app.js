86fa178aa9be55ed02ce60f8590a6c60297f8643
Adrian Bumbu <adibumbu91@gmail.com>
2024-07-30 18:32:55 +0300

    remove unecessary files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Servește fișiere statice din directorul 'public'
+// set the static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) =>
